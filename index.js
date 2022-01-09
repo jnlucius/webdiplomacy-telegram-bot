6933d4ceedb817772d20101a6f1be2fc8cd89427
Lucius <lucius.junius.509@gmail.com>
2022-01-09 23:16:22 +0500

    deleting previous messages in the channel

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { getNewgameList } = require("./utility/data.js");
 const {Telegraf} = require('telegraf');
 require('dotenv').config();
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+let msgInChatID = [];
+
 //for looping
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,17 +13,32 @@ function delay(ms) {
 
 async function loop() {
   while (true) {
+
+    if(!msgInChatID.length == 0){
+        //console.log("Elements in array: " + msgInChatID.length);
+        for (msg of msgInChatID){
+            bot.telegram.deleteMessage(process.env.CHANNEL_ID, msg);
+            //console.log("Deleted message");
+        }
+        msgInChatID = [];
+        //console.log("Elements in array: " + msgInChatID.length);
+    }
+
     const games = await getNewgameList('https://webdiplomacy.ru/gamelistings.php?page-games=1&gamelistType=New');
 
     for (game of games){
-        bot.telegram.sendMessage(process.env.CHANNEL_ID, `"${game.title}"\n\nPhase: ${game.timePerPhase}\nMap: ${game.detail}\n${game.players}\nLink: ${game.link}`);
+        bot.telegram.sendMessage(process.env.CHANNEL_ID, `"${game.title}"\n\nPhase: ${game.timePerPhase}\nMap: ${game.detail}\n${game.players}\nLink: ${game.link}`).then((m) => {
+            msgInChatID.push(m.message_id);
+            //console.log("Pushed msg id into the array. Id: " + m.message_id);
+        })
+       
     } 
-
+    //5 minutes interval
     await delay(300000);
   }
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+
 
 (
     async function main() {
